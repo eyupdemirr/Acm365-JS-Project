d0944014a50e1bce44272f6e648d21adb34b7f60
EyupDmir <eyupdmir50@gmail.com>
2022-12-20 01:35:26 +0300

    Model,Gallery,Footer,ResponsiveDesign eklendi.

--- app.js
+++ app.js
@@ -123,3 +123,16 @@ currentProductSizes.forEach((size, index) => {
     size.style.color = "white";
   });
 });
+
+//*Payment Services
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
